e6dc1d1b758a3a2d43ce5aa4a44858d1472e32bf
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-02-26 14:25:04 +0200

    install lightbox

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', itemsMarkup)
+
+function createGalleryItems (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>`
+    }).join('')
+};
+
+galleryContainer.addEventListener("click", onImageClick)
+
+function onImageClick(evt) {
+    evt.preventDefault()
+
+    if(evt.target.nodeName !== "IMG") {
+        return;
+    }
+
+    const instance = basicLightbox.create(`
+        <img src="${evt.target.dataset.source}" width="800" height="600">
+    `)
+    instance.show()
+}
