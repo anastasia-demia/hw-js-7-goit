d8a09dddca63e5e8ee01e5fd1683c8eba79423fa
anastasia-demia <109302069+anastasia-demia@users.noreply.github.com>
2023-02-26 20:20:58 +0200

    task 2 in process

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,37 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('afterbegin', itemsMarkup)
+
+function createGalleryItems (galleryItems) {
+    return galleryItems.map(({preview, original, description}) => {
+        return `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </div>`
+    }).join('')
+};
+
+function onImageClick(event) {
+    event.preventDefault()
+
+    
+}
+
+var lightbox = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionsData:"alt",
+    captionPosition:'bottom',
+    captionDelay: 250,
+});
